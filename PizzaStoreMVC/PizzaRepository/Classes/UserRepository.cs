a1cb630d3525457d476aeae757599b14b22e7105
mitalijain24 <jmitali996@gmail.com>
2021-09-16 22:23:11 +0530

    Created Entity-Framework Data Layer

---
+++ PizzaStoreMVC/PizzaRepository/Classes/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using PizzaDbData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaRepository.Classes
+{
+   public class UserRepository : Interfaces.IUserRepository
+    {
+        private PizzaDb db;
+        public UserRepository(PizzaDb db)
+        {
+            this.db = db;
+        }
+        public void AddNewUser(User objuser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUser()
+        {
+            return db.Users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            if (id > 0)
+            {
+             var data = db.Users.Where(d => d.id == id).FirstOrDefault();
+             return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public string DeleteUserById(int id)
+        {
+            var logindata = db.Logins.Where(s=>s.user_id==id).FirstOrDefault();
+            db.Logins.Remove(logindata);
+            Save();
+            var user = db.Users.Find(id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                Save();
+                return "Successfully deleted";
+            }
+            else
+                return "User does not exist!!";
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
